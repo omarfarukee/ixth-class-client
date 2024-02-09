8f1afbdbf3b38fcde72fb20fc121e51f0cfe6498
omarfarukee <umarfaruque1920@gmail.com>
2024-02-09 07:44:27 +0600

    set projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+  // theme: {
+  //   extend: {
+  //     backgroundImage: {
+  //       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+  //       "gradient-conic":
+  //         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  //     },
+  //   },
+  // },
+  daisyui: {
+    themes: ["cupcake", "dark", "cmyk", "lofi", "retro"],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
